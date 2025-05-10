2bfb38e4baa1238323149eeed5c001b32911de97
chalanichanchala22 <chalanichanchala2000@gmail.com>
2025-05-11 00:15:54 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,12 +8,7 @@ const footer = () => {
     <footer className="footer">
       <div className="container">
         <p>&copy; {currentYear} Movie Explorer. All rights reserved.</p>
-        <div className="footer-links">
-          <a href="#">About</a>
-          <a href="#">Privacy Policy</a>
-          <a href="#">Terms of Service</a>
-          <a href="#">Contact</a>
-        </div>
+      
       </div>
     </footer>
   );
